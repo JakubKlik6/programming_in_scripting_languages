038c6726abda060b1d1a649a4be1676d0d5f397c
Jakub Klik <Jakub.klik6@gmail.com>
2023-04-09 20:29:33 +0200

    Update random_number_game.py

--- random_number_game.py
+++ random_number_game.py
@@ -1,16 +1,26 @@
-print("Social security number check\n")
+import random
+
+print("Hello player! Welcome to Try to guess the number game!\n")
 print("If you want to exit the game choose 0\n")
 
-userInput = input("enter your social security number... ")
+userInput = input("Guess the number... ")
+number = random.randint(1, 1000)
 
-if len(userInput) == 9:
-    while True:
-        try:
-            userInputAsNumber = int(userInput)
-
-        except:
-            print("Wrong value!")
+while True:
+    try:
+        userInputAsNumber = int(userInput)
+        if userInputAsNumber == 0:
+            print("\nGame over\n")
+            break
+        if userInputAsNumber == number:
+            print("You won")
+            break
+        elif userInputAsNumber < number:
+            print("A little bit more")
             userInput = input("Guess the number... ")
-else:
-    print("Wrong input!")
-
+        elif userInputAsNumber > number:
+            print("A little bit less")
+            userInput = input("Guess the number... ")
+    except:
+        print("Wrong value!")
+        userInput = input("Guess the number... ")
